0c7694a6e28d5234de2fb1780619d4618d3d01b7
10994015 <a0938599191@gmail.com>
2022-01-02 22:55:36 +0800

    0102

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle('sticky', window.scrollY > 0);
+    const upBtn = this.document.getElementsByClassName('scroll-up-btn')[0];
+    upBtn.classList.toggle('active', window.scrollY > 500);    
 });
 
